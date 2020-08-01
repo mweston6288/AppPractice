7edeac34c2b3f9ec96368c51637f0e7f4be37a20
felixcloud6288 <mweston6288@knights.ucf.edu>
2020-08-01 10:32:32 -0400

    Finished the project in C

--- BinaryToDecimal/binaryToDecimal.c
+++ BinaryToDecimal/binaryToDecimal.c
@@ -4,6 +4,7 @@
 int main(){
 	int size;
 	char *array;
+	int decimal = 0;
 	// get size of binary from user.
 	// if user input is not a number, pritn invalid and ask again
 	printf("Number of bits: ");
@@ -18,6 +19,19 @@ int main(){
 	// get binary string
 	printf("Enter your binary string: ");
 	fgets(array, size+1, stdin);
-	printf("%s", array);
+
+	// read each character one at a time.
+	// if any value is not 1 or 0, string is invalid
+	for (int i = 0; i < size; i++){
+		if (array[i] != '1' && array[i] != '0'){
+			printf("Invalid string\n");
+			return 0;
+		// left shift decimal and then add the new bit to the end
+		} else{
+			decimal <<= 1;
+			decimal += (int)array[i] - '0';
+		}
+	}
+	printf("%d\n", decimal);
 	free(array);
 }
